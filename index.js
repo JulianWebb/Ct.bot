bc3f08a647fa371a538a05ece784740198cb6fa3
Jasper <realmctbf@gmail.com>
2020-08-30 13:19:08 -0700

    :pencil: Added stout/io-out logger

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const Discord = require('discord.js');
+const logger = require('./logger.js');
 const docs = require('./docs.js');
+
+const config = require('./config.json');
+const token = config.token;
 
 // Initialize documentation
 docs.clone();
@@ -7,4 +11,11 @@ docs.clone();
 const documentation = docs.parse();
 
 const client = new Discord.Client();
+client.prefix = config.prefix;
 
+client.on('ready', () => {
+    logger.info(`[Ct.bot v${require('./package.json').version} started at ${new Date()}]`)
+    logger.success('Online!');
+})
+
+client.login(token);
