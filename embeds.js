16131ae0508abe0b38cf216dacd32082da7016fa
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-08-30 16:59:30 -0700

    :book: Add prettier

--- embeds.js
+++ embeds.js
@@ -3,18 +3,18 @@ const { Message } = require('discord.js');
 const MessageEmbed = require('discord.js').MessageEmbed;
 
 module.exports = {
-    info(color, title, description, footer) {
-        return new MessageEmbed()
-        .setTitle(title)
-        .setColor(color)
-        .setDescription(description)
-        .setFooter(footer)
-    },
-    doc(color, title, description, link) {
-        return new MessageEmbed()
-        .setTitle(title)
-        .setColor(color)
-        .setDescription(description)
-        .addField('Read More..', link);
-    }
-}
+  info(color, title, description, footer) {
+    return new MessageEmbed()
+      .setTitle(title)
+      .setColor(color)
+      .setDescription(description)
+      .setFooter(footer);
+  },
+  doc(color, title, description, link) {
+    return new MessageEmbed()
+      .setTitle(title)
+      .setColor(color)
+      .setDescription(description)
+      .addField('Read More..', link);
+  },
+};
--- logger.js
+++ logger.js
@@ -9,55 +9,51 @@ const CREATED_AT = timestamp('DD-MM-YYYY_HH-mm-ss');
 
 // Initialize logs
 if (!fs.existsSync(LOGS_DIR)) {
-    fs.mkdirSync(LOGS_DIR);
+  fs.mkdirSync(LOGS_DIR);
 }
 
 log4js.configure({
-    appenders: {
-        application: {
-            type: 'file',
-            filename: `./logs/${CREATED_AT}`
-        }
+  appenders: {
+    application: {
+      type: 'file',
+      filename: `./logs/${CREATED_AT}`,
     },
-    categories: {
-        default: {
-            appenders: ['application'],
-            level: 'debug'
-        }
-    }
-})
+  },
+  categories: {
+    default: {
+      appenders: ['application'],
+      level: 'debug',
+    },
+  },
+});
 
 const fileLogger = log4js.getLogger();
 
 module.exports = {
-    saveToLog: true,
-    createdAt: CREATED_AT,
-    info(message) {
-        console.log(
-            chalk.bold.blue('[Info]') + chalk.reset(' ' + message)
-        )
-        this.writeOut('[Info] ' + message)
-    },
-    warn(message) {
-        console.log(
-            chalk.bold.yellow('[Warn]') + chalk.reset(' ' + message)
-        )
-        this.writeOut('[Warn] ' + message)
-    },
-    fail(message) {
-        console.log(
-            chalk.bold.bgRed.white('[Error]') + chalk.reset.bold(' ' + message)
-        )
-        this.writeOut('[Error] ' + message)
-    },
-    success(message) {
-        console.log(
-            chalk.bold.bgGreen.white('[Success]') + chalk.reset.bold(' ' + message)
-        )
-        this.writeOut('[Success] ' + message)
-    },
-    writeOut(data) {
-        if (!this.saveToLog) return;
-        fileLogger.debug(data)
-    }
-}
+  saveToLog: true,
+  createdAt: CREATED_AT,
+  info(message) {
+    console.log(chalk.bold.blue('[Info]') + chalk.reset(' ' + message));
+    this.writeOut('[Info] ' + message);
+  },
+  warn(message) {
+    console.log(chalk.bold.yellow('[Warn]') + chalk.reset(' ' + message));
+    this.writeOut('[Warn] ' + message);
+  },
+  fail(message) {
+    console.log(
+      chalk.bold.bgRed.white('[Error]') + chalk.reset.bold(' ' + message),
+    );
+    this.writeOut('[Error] ' + message);
+  },
+  success(message) {
+    console.log(
+      chalk.bold.bgGreen.white('[Success]') + chalk.reset.bold(' ' + message),
+    );
+    this.writeOut('[Success] ' + message);
+  },
+  writeOut(data) {
+    if (!this.saveToLog) return;
+    fileLogger.debug(data);
+  },
+};
